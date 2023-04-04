b484613a5e20a9e5f90ddc1ceb98a282b06a9e58
bijoymamud <bijoymamud.09@gmail.com>
2023-04-04 14:17:52 +0600

    added buy now button

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -3,7 +3,7 @@ import Features from '../Features/Features';
 
 const PriceCard = ({ price }) => {
     return (
-        <div className='mt-5 mb-5  mx-2 rounded-md bg-amber-500  text-center p-5 drop-shadow-md '>
+        <div className='mt-5 mb-5  mx-2 rounded-md bg-amber-500  text-center p-5 drop-shadow-md flex flex-col '>
 
             <h2>
                 <span className='text-5xl text-white font-extrabold font-mono'>${price.price}</span>
@@ -11,7 +11,7 @@ const PriceCard = ({ price }) => {
             </h2>
 
             <h2 className='text-3xl font-bold font-mono '>{price.name}</h2>
-            <div className='text-start mt-10'>
+            <div className='text-start mt-10    '>
                 <p className='font-mono font-bold text-xl underline underline-offset-8 mb-3' >Features:</p>
 
                 {
@@ -23,9 +23,12 @@ const PriceCard = ({ price }) => {
                             feature={feature}
 
                         ></Features>)
-                    // className='font-mono text-cyan-50 ' >
+
+
                 }
+
             </div>
+            <button className=' mt-auto w-full bg-green-600 py-2 rounded-md font-mono font-semibold'>Buy Now</button>
 
         </div>
     );
